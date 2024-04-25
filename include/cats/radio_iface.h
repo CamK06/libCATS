28ee3f816944b58e6c353c5c146282c25ec57dad
Cam K. <a@a>
2024-04-25 11:16:02 -0400

    Document radio_iface

--- include/cats/radio_iface.h
+++ include/cats/radio_iface.h
@@ -10,7 +10,24 @@
 #include <stdint.h>
 #include <stddef.h>
 
+/**
+ * @brief Encode a packet with CBOR for transmission between a CATS radio and computer
+ * 
+ * @param buf Input and output buffer
+ * @param buf_len Length of input data
+ * @param rssi Receiver RSSI, if unknown or not applicable, set to 0
+ * @return Encoded length
+ */
 uint16_t cats_radio_iface_encode(uint8_t* buf, const size_t buf_len, const float rssi);
+
+/**
+ * @brief Decode a packet with CBOR for transmission between a CATS radio and computer
+ * 
+ * @param buf Input and output buffer
+ * @param buf_len Length of input data
+ * @param rssi_out Pointer to store RSSI, set to NULL if not needed
+ * @return Decoded length or CATS_FAIL on failure
+ */
 int cats_radio_iface_decode(uint8_t* buf, const size_t buf_len, float* rssi_out);
 
 #endif // CATS_RADIO_IFACE_H
