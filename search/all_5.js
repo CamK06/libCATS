0226c2fc131d8c659824bf4ccb14231798fbb58d
CamK06 <CamK06@users.noreply.github.com>
2024-05-16 15:17:23 +0000

    deploy: 107f204e2f11afd3e6b0bf370cc571e0aa2a09ab

--- search/all_5.js
+++ search/all_5.js
@@ -1,10 +1,12 @@
 var searchData=
 [
-  ['nodeinfo_5fi16_0',['nodeinfo_i16',['../structnodeinfo__i16.html',1,'']]],
-  ['nodeinfo_5fi32_1',['nodeinfo_i32',['../structnodeinfo__i32.html',1,'']]],
-  ['nodeinfo_5fi8_2',['nodeinfo_i8',['../structnodeinfo__i8.html',1,'']]],
-  ['nodeinfo_5fu16_3',['nodeinfo_u16',['../structnodeinfo__u16.html',1,'']]],
-  ['nodeinfo_5fu32_4',['nodeinfo_u32',['../structnodeinfo__u32.html',1,'']]],
-  ['nodeinfo_5fu8_5',['nodeinfo_u8',['../structnodeinfo__u8.html',1,'']]],
-  ['nodeinfo_5fvoltage_6',['nodeinfo_voltage',['../structnodeinfo__voltage.html',1,'']]]
+  ['nodeinfo_5fbool_0',['nodeinfo_bool',['../structnodeinfo__bool.html',1,'']]],
+  ['nodeinfo_5ff32_1',['nodeinfo_f32',['../structnodeinfo__f32.html',1,'']]],
+  ['nodeinfo_5fi16_2',['nodeinfo_i16',['../structnodeinfo__i16.html',1,'']]],
+  ['nodeinfo_5fi32_3',['nodeinfo_i32',['../structnodeinfo__i32.html',1,'']]],
+  ['nodeinfo_5fi8_4',['nodeinfo_i8',['../structnodeinfo__i8.html',1,'']]],
+  ['nodeinfo_5fu16_5',['nodeinfo_u16',['../structnodeinfo__u16.html',1,'']]],
+  ['nodeinfo_5fu32_6',['nodeinfo_u32',['../structnodeinfo__u32.html',1,'']]],
+  ['nodeinfo_5fu8_7',['nodeinfo_u8',['../structnodeinfo__u8.html',1,'']]],
+  ['nodeinfo_5fvoltage_8',['nodeinfo_voltage',['../structnodeinfo__voltage.html',1,'']]]
 ];
